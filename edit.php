425ef8dcc62710c8b74a8365b939a577e98b6558
victor FernandezF <victorfernandezflores2@gmail.com>
2022-04-29 12:05:00 +0200

    Editar con fotacas

--- edit.php
+++ edit.php
@@ -1,5 +1,6 @@
 <?php
 include("connection.php");
+include("funciones.php");
 
 if (!isset($_GET['id'])) {
     header("Location: index.php");
@@ -34,10 +35,70 @@ if(isset($_POST["edit"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
     $formato = $_POST['formato'];
     $descripcion = $_POST['descripcion'];
     $plataforma1 = $_POST['plataforma'];
+    $imagen = nombreMasBarraBaja($nombre);
 
     $uppernombre = strtoupper($nombre);  
     $plataforma = strtoupper($plataforma1);  
-    $query = "UPDATE juegos SET nombre = '$nombre', genero = '$genero', instalado = '$instalado', espacio = '$espacio', jugadores = '$jugadores', formato = '$formato', descripcion = '$descripcion', plataforma = '$plataforma' WHERE id = $id";
+    
+
+    $target_dir = "images/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    
+    // Check if image file is a actual image or fake image
+    
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+      } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    
+    
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      unlink($target_file);
+      //echo "Sorry, file already exists.";
+      $uploadOk = 1;
+    }
+    
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
+    }
+    
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+    }
+    
+    
+    
+    
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+      } else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+    }
+    rename($target_file, "images/$imagen.jpg");
+     
+    //exit();
+    
+    
+    
+    $query = "UPDATE juegos SET nombre = '$nombre', genero = '$genero', instalado = '$instalado', espacio = '$espacio', jugadores = '$jugadores', formato = '$formato', descripcion = '$descripcion', plataforma = '$plataforma', imagen ='$imagen' WHERE id = $id";
     mysqli_query($conn, $query);
     //echo "<script>window.history.back()</script>";
     //echo "<script>window.history.back()<script>";
@@ -114,6 +175,10 @@ if(isset($_POST["edit"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
                     <fieldset class="noborde">
                         <label for="plataforma">PLATAFORMA</label><br>
                         <input class="input" type="text" id="plataforma" name="plataforma" value="PS4" value="<?php echo $plataforma ?>">
+                    </fieldset>
+                    <fieldset class="noborde">
+                        <label for="plataforma">Select image to upload:</label><br>
+                        <input type="file" name="fileToUpload" id="fileToUpload">
                     </fieldset>
 
                     <fieldset class="noborde">
