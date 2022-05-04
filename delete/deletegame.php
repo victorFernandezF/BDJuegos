3afa97f74fb77200b1951ac11cc85b6ca6050c51
victor FernandezF <victorfernandezflores2@gmail.com>
2022-05-04 12:16:21 +0200

    Añadidas funcionalidad de eliminar juegos

---
+++ delete/deletegame.php
@@ -0,0 +1,30 @@
+<?php
+include("../connection.php");
+include("../funciones.php");
+
+if (!isset($_GET['id'])) {
+  header("Location: ../index.php");
+  exit();
+} 
+
+$id = $_GET['id'];
+
+$query = "DELETE FROM juegos WHERE id = $id";
+//echo $query;
+$result = mysqli_query($conn, $query);
+header('Location: deletelist.php');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- delete/deletelist.php
+++ delete/deletelist.php
@@ -1,41 +1,8 @@
 <?php
-include("connection.php");
-include("funciones.php");
+include("../connection.php");
+include("../funciones.php");
 
- if(isset($_POST["ver"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
-    $todos = $todos = isset($_POST['todos']) ? "si" : "no";
-    $where = "WHERE 1";
-    $plataforma = $plat = isset($_POST['plataforma']) ? "si" : "no";
-    $genero = $plat = isset($_POST['genero']) ? "si" : "no";
-    $jugadores = $plat = isset($_POST['jugadores']) ? "si" : "no";
-    $instalado = $plat = isset($_POST['instalado']) ? "si" : "no";
-
-    if($todos == 'si'){
-        $where = $where;  
-      };  
-
-    if($plataforma == 'si'){
-      $plataforma2 = $_POST['plataforma2'];
-      $where = "$where AND plataforma = '$plataforma2'";  
-    };
-
-    if($genero == 'si'){
-        $genero2 = $_POST['genero2'];
-        $where = "$where AND genero = '$genero2'";  
-      };
-      
-      if($jugadores == 'si'){
-        $jugadores2 = $_POST['jugadores2'];
-        $where = "$where AND jugadores = '$jugadores2'";  
-      };
-
-      if($instalado == 'si'){
-        $instalados2 = $_POST['instalados2'];
-        $where = "$where AND instalado = '$instalados2'";  
-      };
-
-
-    $query = "SELECT * FROM juegos $where ORDER BY nombre ";
+    $query = "SELECT * FROM juegos ORDER BY nombre";
     //echo $query;
     $arreglo = array();
     $arreglonombre = array();
@@ -53,9 +20,6 @@ $filingas = 6;
 $maximo = count($arreglo);
 $filas = $maximo / $filingas;
 
-
-
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -67,7 +31,7 @@ $filas = $maximo / $filingas;
 </head>
 <body>
 <?php
-include("connection.php");
+include("../connection.php");
 //include("funciones.php");
 ?>
 <!DOCTYPE html>
@@ -76,7 +40,7 @@ include("connection.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="../estilos.css">
     <title>Ver Juegos</title>
 </head>
 <body>
@@ -87,7 +51,7 @@ include("connection.php");
             </div>    
 
             <div class="menu">
-                <?php include("menuses.php") ?>
+                <?php include("../menuses.php") ?>
             </div>
             <div class="texto-tabla">
                 <div class="divdetabla">
@@ -95,14 +59,14 @@ include("connection.php");
     <table class="cr_table2 table table-bordered, tablamatch">
         <thead>
           <tr>
-            <th colspan ="6">JUEGOS INSTALADOS PS4</th>
+            <th colspan ="6">ELIMINAR JUEGO</th>
           </tr>
         </thead>
         <tbody>
             <?php while ($i < $filas) { ?>
                 <tr>
                     <?php while(($cada <= $fin )&&($cada < $maximo)){ ?>
-                        <td><a href="detalles.php?img=<?php echo $arreglo[$cada]?>"><img class="persona" src="images/<?php echo $arreglo[$cada]; ?>.jpg" title="<?php echo $arreglonombre[$cada]; ?>" ></a></td>
+                        <td><a href="areyousure.php?img=<?php echo $arreglo[$cada]?>"><img class="persona" src="../images/<?php echo $arreglo[$cada]; ?>.jpg" title="<?php echo $arreglonombre[$cada]; ?>" ></a></td>
                         <?php $cada++;
                     
                     } $fin = $fin+6?>
